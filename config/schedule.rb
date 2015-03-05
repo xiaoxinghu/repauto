fa4ea64cc5f8745c0aa386b5762e267037c3e13c
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-03-05 13:40:01 +1300

    ignore and change whenever task

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 
-set :environment, "development"
+set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 1.hours do
