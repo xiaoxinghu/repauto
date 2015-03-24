5ef263e706cd0fb614cc19f7e5a4d2e1e81e543c
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-03-24 13:33:36 +1300

    sort result by number of tests

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,11 +4,20 @@ class ProjectsController < ApplicationController
 
   def show
     @show_num = 2
+    @min_duration = 30.minutes
     @matrix = {}
     @project = Project.find(params[:id])
     categories = @project.test_runs.select(:name).distinct.map(&:name)
     categories.each do |c|
-      @matrix[c] = @project.test_runs.where.not(end: nil).where(name: c).select { |tr| (tr.end - tr.start) > 30.minutes }.sort_by!(&:start).reverse!.take(@show_num)
+      @matrix[c] = @project.test_runs
+                   .where.not(end: nil)
+                   .where(name: c)
+                   .select { |tr| (tr.end - tr.start) > @min_duration }
+                   .sort_by!(&:start)
+                   .reverse!.take(@show_num)
     end
+    @sorted_c = @matrix
+                .sort_by { |_k, v| v[0] ? -v[0].count : 1 }
+                .map { |x| x[0] }
   end
 end
