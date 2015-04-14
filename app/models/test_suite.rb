57c2654b4d83c2d14c59ad46d4f50ab3cebf5e20
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-04-14 12:29:19 +1200

    add exception handle

--- app/models/test_suite.rb
+++ app/models/test_suite.rb
@@ -6,27 +6,32 @@ class TestSuite < ActiveRecord::Base
   def self.sync(test_run)
     found = 0
     ls_xml(File.join(test_run.full_path, 'allure')).each do |xml|
-      found += 1
-      content = Nokogiri::XML(http_get(xml).body)
-      xml.slice! test_run.full_path
+      begin
+        found += 1
+        content = Nokogiri::XML(http_get(xml).body)
+        p = xml.slice test_run.full_path
 
-      next if TestSuite.where(test_run: test_run, path: xml).any?
-      content.xpath("xmlns:test-suite").each do |ts|
-        test_suite = TestSuite.find_or_create_by(test_run: test_run, path: xml)
-        test_suite.name = ts.xpath('name').first.content
-        test_suite.start = Time.zone.at(ts['start'].to_i / 1000)
-        test_suite.end = Time.zone.at(ts['stop'].to_i / 1000)
-        test_suite.save
+        next if TestSuite.where(test_run: test_run, path: p).any?
+        content.xpath("xmlns:test-suite").each do |ts|
+          test_suite = TestSuite.find_or_create_by(test_run: test_run, path: p)
+          test_suite.name = ts.xpath('name').first.content
+          test_suite.start = Time.zone.at(ts['start'].to_i / 1000)
+          test_suite.end = Time.zone.at(ts['stop'].to_i / 1000)
+          test_suite.save
 
-        if not test_run.end or test_suite.end > test_run.end
-          test_run.end = test_suite.end
-          test_run.save
+          if not test_run.end or test_suite.end > test_run.end
+            test_run.end = test_suite.end
+            test_run.save
+          end
+          if not test_run.start or test_suite.start < test_run.start
+            test_run.start = test_suite.start
+            test_run.save
+          end
+          TestCase.sync(test_suite, ts)
         end
-        if not test_run.start or test_suite.start < test_run.start
-          test_run.start = test_suite.start
-          test_run.save
-        end
-        TestCase.sync(test_suite, ts)
+      rescue Nokogiri::XML::XPath::SyntaxError => e
+        logger.error "syntax error when parsing #{xml}"
+        raise e
       end
     end
   end
