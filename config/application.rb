3165b7fc9b60c1a2ac22e9ace820abba9cd7f86a
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-08-06 11:25:14 +1200

    get rid of sidekiq

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module Tereport
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.active_job.queue_adapter = :sidekiq
   end
 end
