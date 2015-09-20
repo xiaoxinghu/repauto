75b4ac5790b190a3753dad710bd6b970e4aadebc
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-09-21 10:06:53 +1200

    fix bug

--- config/schedule.rb
+++ config/schedule.rb
@@ -23,5 +23,5 @@ set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
 every 1.hours do
-  rake "report:sync"
+  rake "data:sync"
 end
