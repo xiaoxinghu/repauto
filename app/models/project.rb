8ba8cbae3c06a8dc9f34e987b318254124364239
xiaoxing <dawnstar.hu@gmail.com>
2015-10-07 17:06:17 +1300

    clean up

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,11 +5,6 @@ class Project
   has_many :test_cases
 
   field :path, type: String
-  # field :project, as: :name, type: String
-  #
-  # def test_runs
-  #     TestRun.from self
-  # end
 
   def run_types
     test_runs.exists(archived_at: false).distinct('type')
--- app/models/test_suite.rb
+++ app/models/test_suite.rb
@@ -9,13 +9,4 @@ class TestSuite
   field :file_name, type: String
   field :start, type: Time
   field :stop, type: Time
-
-  # def self.from(test_run)
-  #   where(path: %r{^#{test_run.path}/})
-  # end
-
-  # def test_cases
-  #   TestCase.from self
-  # end
-
 end
