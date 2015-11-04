b421d19251934fad455772329155661b0d058739
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-11-04 16:14:58 +1300

    change settings

--- config/initializers/data_cleanup.rb
+++ config/initializers/data_cleanup.rb
@@ -4,9 +4,9 @@ end
 
 DataCleanup.configure do |config|
   # keep last 100 runs per run type
-  config.keep_amount = 10
+  config.keep_amount = 20
   # in days
-  config.max_life = 30
+  config.max_life = 14
   # keep the numbers
   config.keep_report = true
 end
