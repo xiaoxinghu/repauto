df178d5da5b6e7dc90fa0cf96ee9afa7e0b217a3
Xiaoxing Hu <dawnstar.hu@gmail.com>
2015-11-06 11:12:26 +1300

    fix jump to detail button on summary page

--- ui/components/Trend/Card.jsx
+++ ui/components/Trend/Card.jsx
@@ -1,15 +1,35 @@
 import React, {Component, PropTypes} from 'react';
 import TrendGraph from './Graph';
+import { pushState } from 'redux-router';
+import { connect } from 'react-redux';
 import _ from 'lodash';
+import { fetch, filter, invalidate } from '../../modules/TestRun';
 
+@connect(
+  state => ({
+    path: state.router.location.pathname
+  }),
+  {filter, pushState, invalidate}
+)
 export default class Card extends Component {
+  _jumpToDetail(name) {
+    const{invalidate, filter, pushState, path} = this.props;
+    console.info('jump to', name);
+    invalidate();
+    filter(name);
+    pushState(null, `${path}/runs`);
+  }
+
   render() {
     const {title, data} = this.props;
     var heading = [
       <h3 key='heading' className='panel-title pull-left'>
         {title}
       </h3>,
-      <a key='search' href={this.props.url} className='btn btn-primary pull-right'>
+      <a key='search'
+        href={this.props.url}
+        className='btn btn-primary pull-right'
+        onClick={() => this._jumpToDetail(title)}>
         <i className='fa fa-search' />
       </a>
     ];
